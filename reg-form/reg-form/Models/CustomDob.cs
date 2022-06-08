7131ce7752cda0b941b22d661ea97b701e123ed8
sanzidul-islam <sanzidulislam16@gmail.com>
2022-06-09 00:52:37 +0600

    2nd

---
+++ reg-form/reg-form/Models/CustomDob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace reg_form.Models
+{
+    public class CustomDob:ValidationAttribute
+
+    {
+        int _minimumAge;
+        public CustomDob(int minimumAge)
+        {
+            _minimumAge = minimumAge;
+        }
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if(DateTime.TryParse(value.ToString(), out date))
+                
+              {
+                return date.AddYears(_minimumAge) < DateTime.Now;
+
+            }
+            return false;
+        }
+
+    }
+}
--- reg-form/reg-form/Models/student.cs
+++ reg-form/reg-form/Models/student.cs
@@ -10,12 +10,26 @@ namespace reg_form.Models
     {
         [Required]
         [StringLength(40)]
+        [RegularExpression("^[a-zA-Z]*$")]
         public string Name { set; get; }
         [Required]
         [Range(0,50)]
+        
         public string Id { set; get; }
         [Required]
+        [CustomDob(18)]
+        [DataType(DataType.Date),DisplayFormat]
         public string Dob { set; get; }
+        [Required]
+        [MinLength(9,ErrorMessage ="Minium 8 character is required")]
+        public string password { set; get; }
+        [Required]
+        [Compare("password", ErrorMessage ="password does not match,try again !!")]
+        public string confirmpassword { set; get; }
+        [Required]
+        [EmailAddress]
+        public string email { set; get; }
+
 
     }
 }
